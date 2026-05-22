14758973e4017ab13e80b54450197455327e4d54
MOorrRRrr <ceciliamaura04@gmail.com>
2026-05-22 20:41:23 +0700

    Web v1

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// //toggle class active
+// const navbarNav = document.querySelector
+
+// SHOW MENU
+const showMenu = (navId, toggleId) => {
+   const nav = document.getElementById(navId),
+         toggle = document.getElementById(toggleId)
+
+   toggle.addEventListener('click', () => {
+      nav.classList.toggle('show-menu')
+      toggle.classList.toggle('show-icon')
+   })
+}
+showMenu('nav-menu','nav-toggle')
